ed9cbf6b9113efa9d083b713b7f33c351f278ec8
Josh Newsome <jn799421@gmail.com>
2024-12-10 20:13:22 -0500

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -9,17 +9,34 @@ export default async function HomePage() {
   }
 
   return (
-    <div>
-      <h1>Name Selection</h1>
-      <ul>
-        {names.length > 0 ? (
-          names.map((nameEntry) => (
-            <li key={nameEntry.id}>{nameEntry.firstname}</li>
-          ))
-        ) : (
-          <p>No names found or an error occurred.</p>
-        )}
-      </ul>
-    </div>
+    
+    
+
+<div class="dropdown">
+<button onlick="myFunction()" class="dropbtn">Dropdown</button>
+<div id="myDropdown" class="dropdown-content">
+  <a href="#">Link 1</a>
+  <a href="#">Link 2</a>
+  <a href="#">Link 3</a>
+</div>
+</div>
   );
+
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
 }
